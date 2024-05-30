efb822064652793af9bddc7c8dbe5a415d5460a2
Finomnis <finomnis@gmail.com>
2024-05-30 12:36:28 +0200

    Update to eh-1; minor rework

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@ pub mod _deps {
     pub use embedded_hal;
     pub use imxrt_hal;
     pub use imxrt_ral;
-    pub use nb;
 }
 
 /// Registers the UART panic handler.
@@ -31,10 +30,9 @@ macro_rules! register {
         fn panic(info: &::core::panic::PanicInfo) -> ! {
             use ::core::fmt::Write as _;
 
-            use $crate::_deps::embedded_hal::serial::Write as _;
+            use $crate::_deps::embedded_io::Write as _;
             use $crate::_deps::imxrt_hal as hal;
             use $crate::_deps::imxrt_ral as ral;
-            use $crate::_deps::nb::block;
 
             use hal::ccm;
             use hal::lpuart::{Baud, Direction, Lpuart, Pins, Watermark};
@@ -75,10 +73,11 @@ macro_rules! register {
                 fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
                     for &b in s.as_bytes() {
                         if b == b'\n' {
-                            let _ = block!(self.uart.write(b'\r'));
+                            let _ = self.uart.write(b'\r');
                         }
-                        let _ = block!(self.uart.write(b));
+                        let _ = self.uart.write(b);
                     }
+                    self.uart.flush();
                     Ok(())
                 }
             }
